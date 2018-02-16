d9bf7038a4c5c7992d6b36b5bc8186083b6ce79d
senthur <senthur@Kids-Laptop.fios-router.home>
2018-02-16 17:12:31 -0500

    Adding Flight Confirmation

---
+++ POMDemo/src/com/demoaut/newtours/Pages/FlightConfirmation.java
@@ -0,0 +1,16 @@
+package com.demoaut.newtours.Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class FlightConfirmation {
+
+	@FindBy(css="body > div > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(4) > td > table > tbody > tr:nth-child(1) > td:nth-child(2) > table > tbody > tr:nth-child(3) > td > p > font > b > font:nth-child(2)")
+	public WebElement message;
+	
+	public FlightConfirmation(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+}
